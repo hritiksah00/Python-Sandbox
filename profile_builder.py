4ad4111bc902c09de5fe8aab2fcc9fb9f414ff04
hritiksah00 <ritiks.2018@kfabusinessschool.edu.np>
2026-07-09 16:58:26 +0545

    Profile Build

---
+++ profile_builder.py
@@ -0,0 +1,6 @@
+name = "John"
+age = 30
+height_in_meters = 1.75
+is_learning_python = True
+
+print(f"My name is {name}, I am {age} years old, my height is {height_in_meters} meters, and it is {is_learning_python} that I am learning Python.")
