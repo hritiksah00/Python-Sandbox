d61cfb8a6799bc5b918787720c45eb9c593bc307
hritiksah00 <ritiks.2018@kfabusinessschool.edu.np>
2026-07-15 19:30:48 +0545

    S3 Bucket Mounted Successfully

---
+++ cloud_manager/storage.py
@@ -0,0 +1,2 @@
+def mount_drive():
+    print("S3 Bucket mounted successfully.")
---
+++ deploy_infrastructure.py
@@ -0,0 +1,3 @@
+from cloud_manager import storage
+storage.mount_drive()
+print(storage.mount_drive())  # Output: S3 Bucket mounted successfully.
